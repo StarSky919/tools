71d6ce6e12a2e13ef5b4cf13ece7afe2646e49cf
StarSky919 <ss919p@gmail.com>
2021-01-17 03:56:32 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*────Cutom Functions────*/
+
 function getElm1(id) {
     return document.getElementById(id);
 }
@@ -9,3 +11,26 @@ function getElm2(query) {
 function getElm3(query) {
     return document.querySelectorAll(query);
 }
+
+/*────Fade In────*/
+
+setTimeout(function() {
+    getElm2('.row').classList.add('fade-in');
+}, 0);
+
+/*var href = location.href;
+
+var links = [
+    'http://localhost:7700/index.html',
+    'https://starsky919.github.io/tools/index.html',
+    'https://starsky919.gitee.io/tools/index.html'
+];
+
+if (href != links[0] && href != links[1] && href != links[2]) {
+    setTimeout(function() {
+        getElm2('.row').classList.add('fade-in');
+    }, 0);
+} else {
+    getElm2('.row').style.transition = 'none';
+    getElm2('.row').classList.add('fade-in');
+}*/
